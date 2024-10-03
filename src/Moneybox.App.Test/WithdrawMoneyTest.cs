2d89c08107af64eefa78116b2dfcb863f0711200
Michael Ajibowu <banjiajibowu@yahoo.com>
2024-10-03 12:25:04 +0100

    Add Test for Notify Fund in Withdraw Feature

---
+++ src/Moneybox.App.Test/WithdrawMoneyTest.cs
@@ -0,0 +1,42 @@
+﻿using Moneybox.App.DataAccess;
+using Moneybox.App.Domain.Services;
+using Moneybox.App.Features;
+using Moq;
+using NUnit.Framework;
+using System;
+
+namespace Moneybox.App.Test
+{
+    [TestFixture]
+    public class WithdrawMoneyTest
+    {
+        private Mock<INotificationService> _notificationService;
+        private Mock<IAccountRepository> _accountRepository;
+
+        private Mock<User> _subjectUser;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            _subjectUser = new Mock<User>(MockBehavior.Strict);
+            
+            _notificationService = new Mock<INotificationService>();
+
+            //Account with balance at 395m
+            var subjectAccount = new Account(Guid.NewGuid(), _subjectUser.Object, 395m);
+
+            _accountRepository = new Mock<IAccountRepository>();
+            _accountRepository.Setup(x => x.GetAccountById(It.IsAny<Guid>())).Returns(subjectAccount);
+        }
+
+        [Test]
+        public void Test_NotifyCustomerOfLowFund_IfBalanceLessThan200m()
+        {
+            var withdrawMoney = new WithdrawMoney(_accountRepository.Object, _notificationService.Object);
+
+            withdrawMoney.Execute(Guid.NewGuid(), 200);
+
+            _notificationService.Verify(x => x.NotifyFundsLow(It.IsAny<string>()), Times.Once);
+        }
+    }
+}
